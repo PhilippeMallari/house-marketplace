454fc2f6eba45422d10c1bac6dae2095ef41df55
PhilippeMallari <mallari.philippe@gmail.com>
2023-05-10 16:25:21 -0600

    fix memory leak warning / added spinner

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,21 +1,34 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 
 export const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false)
     const [checkingStatus, setCheckingStatus] = useState(true)
+    const isMounted = useRef(true)
 
     useEffect(() => {
-        const auth = getAuth()
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setLoggedIn(true)
-            }
+        if (isMounted) {
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    setLoggedIn(true)
+                }
 
-            setCheckingStatus(false)
-        })
-    })
+                setCheckingStatus(false)
+            })
+        }
+
+        return () => {
+            isMounted.current = false
+        }
+    }, [isMounted])
 
     return { loggedIn, checkingStatus }
+
+    // Protected routes in v6
+    // https://stackoverflow.com/questions/65505665/protected-route-with-firebase
+
+    // Fix memory leak warning
+    // https://stackoverflow.com/questions/59780268/cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks
 }
