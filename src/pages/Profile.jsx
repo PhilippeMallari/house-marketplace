75b8702a25dfbef1e86ffe74f751ab7a74eca7db
PhilippeMallari <mallari.philippe@gmail.com>
2023-05-10 14:31:03 -0600

    user details changing and updating added / toast feature added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,26 +1,71 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { useNavigate, Link } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function Profile() {
   const auth = getAuth()
-  const [formData, setformData] = useState({
+  const [changeDetails, setChangeDetails] = useState(false)
+  const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
   })
 
-  const {ame, email} = formData
+  const {name, email} = formData
 
   const navigate = useNavigate()
 
+  // Logging out
   const onLogout = () => {
     auth.signOut()
     navigate('/')
   }
 
+  const onSubmit = async () => {
+    try {
+      if(auth.currentUser.displayName !== name) {
+        // Update display name in firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        })
+
+        // Update in firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name
+        })
+        toast.success('User details changed', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+      }
+    } catch (error) {
+      toast.error('Could not update profile details')
+    }
+  }
+
+  // Changing the details of the user
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+
   return (
     <div className="profile">
+      <ToastContainer />
       <header className="profileHeader">
         <p className="pageHeader">
           My Profile
@@ -29,6 +74,27 @@ export default function Profile() {
           Logout
         </button>
       </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">
+            Personal details
+          </p>
+          <p className="changePersonalDetails" onClick={() => {
+            changeDetails && onSubmit()
+            setChangeDetails((prevState) => !prevState)
+          }}>
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+
+        <div className="profileCard">
+          <form>
+            <input type="text" id="name" className={!changeDetails ? 'profileName' : 'profileNameActive'} disabled={!changeDetails} value={name} onChange={onChange} />
+            <input type="text" id="email" className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} disabled={!changeDetails} value={email} onChange={onChange} />
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
