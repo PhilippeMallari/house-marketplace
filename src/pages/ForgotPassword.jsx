74856612ef303134484828ed33d571b654512b69
PhilippeMallari <mallari.philippe@gmail.com>
2023-05-11 16:21:14 -0600

    forgot password feature added

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,7 +1,54 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { ToastContainer, toast } from 'react-toastify';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg' 
 
 export default function ForgotPassword() {
+
+  const [email, setEmail] = useState('')
+
+    const onChange = (e) => {
+      setEmail(e.target.value)
+    }
+
+    const onSubmit = async(e) => {
+      e.preventDefault()
+      try {
+        const auth = getAuth()
+        await sendPasswordResetEmail(auth, email)
+        toast.success('Email was sent')
+      } catch (error) {
+        toast.error('Could not send reset email')
+      }
+    }
+
   return (
-    <div>Forgot Password</div>
+    <div className='pageContainer'>
+      <ToastContainer />
+      <header>
+        <p className='pageHeader'>
+          Forgot Password
+        </p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input type="email" placeholder='Email' className='emailInput' id='email' value={email} onChange={onChange} />
+          <Link className='forgotPasswordLink' to='/sign-in'>
+            Sign In
+          </Link>
+
+          <div className="signInBar">
+            <div className="signInText">
+              Send Reset Link
+            </div>
+            <button className="signInButton">
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px'/>
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
   )
 }
