b5042c5fce562a42db94402f55bbf314dbabca01
PhilippeMallari <mallari.philippe@gmail.com>
2023-07-08 16:01:51 -0600

    able to console log single listing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
-
+import Listing from './pages/Listing'
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
           <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
-
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
           {/* For Authenticated users only*/}
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
